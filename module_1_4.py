9d6c643cdc31b2c6139291d2fadd93bb9c8cc3c9
Viktoriya-max <viktoriyakoloskova20@gmail.com>
2024-10-04 22:52:18 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,26 @@
+# organization of the program
+my_string =input("Введите символы: ")
+print("Количество символов:",len(my_string))
+# work with string methods
+#1st exercise
+print(my_string.upper())
+#2st exercise
+print(my_string.lower())
+#3st exercise
+print(my_string.replace(" ",''))
+#4st  exercise
+print(my_string[0])
+#5st exercise
+print(my_string[-1])
+
+
+
+
+
+
+
+
+
+
+
+
